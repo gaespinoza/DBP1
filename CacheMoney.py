b0a26c1565f66a0def30a3d21b2870499972c9a7
Daniel Quiroga <dquiroga@email.wm.edu>
2021-04-15 16:28:32 -0400

    Tabs fix

--- CacheMoney.py
+++ CacheMoney.py
@@ -158,7 +158,7 @@ def course_list():
 		"group by T.course_id, T.sec_id, T.semester, T.year) as TA on S.course_id = TA.course_id and S.sec_id = TA.sec_id and S.semester = TA.semester and S.year = TA.year) as Table1 "\
 		"where Table1.semester=%s and Table1.year=%s"
 
-   	cur.execute(query, (semester, year,))
+	cur.execute(query, (semester, year,))
 
    	for row in cur:
    		print(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10])
