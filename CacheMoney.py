03d0c1bd377ae19b4ff4954c2f5b5eeb687b4126
Daniel Quiroga <dquiroga@email.wm.edu>
2021-04-15 16:54:16 -0400

    time slots

--- CacheMoney.py
+++ CacheMoney.py
@@ -174,9 +174,7 @@ def course_list():
 			print(formating)
 			print(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10])
 	except psycopg2.errors.InvalidTextRepresentation:
-		print("gets here")
-
-
+		print("ERROR: Please input a number for year")
 
 	print("Generate Course List!")
 
